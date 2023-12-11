8aa2a2b06913242a25ad5023ef14d960141282a2
Alec bana <anbana994@gmail.com>
2023-12-10 23:48:27 -0500

    added to the connection list component

---
+++ src/components/ConnectionList/ConnectionList.js
@@ -0,0 +1,20 @@
+// src/components/ConnectionList/ConnectionList.js
+import React from 'react';
+
+function ConnectionList({ connections }) {
+    return (
+        <div className="connection-list">
+            <h2>Connections</h2>
+            <ul>
+                {connections.map((connection) => (
+                    <li key={connection.id}>
+                        <span>{connection.name}</span>
+                        {/* Add more details or actions per connection */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ConnectionList;
