f247b30fef1ef3fab81b3cf6e0a88a06e8be0952
Alec bana <anbana994@gmail.com>
2023-12-12 23:26:30 -0500

    changed port to listen on api calls

--- src/services/connectionService.js
+++ src/services/connectionService.js
@@ -1,7 +1,7 @@
 // src/services/connectionService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/connections'; // Your API endpoint
+const API_URL = 'http://localhost:3000/api/connections'; // Your API endpoint
 
 export const fetchUserConnections = async () => {
     const response = await axios.get(API_URL);
