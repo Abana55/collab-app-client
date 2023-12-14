eb0ebae7b999e63c70a97c9f52e2cce9bde65eed
Alec bana <anbana994@gmail.com>
2023-12-13 23:59:10 -0500

    artwork edit file

---
+++ src/components/ArtworkEdit/ArtworkEdit.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const EditArtworkForm = ({ artwork, onSubmitSuccess }) => {
+    const [title, setTitle] = useState(artwork.title);
+    const [description, setDescription] = useState(artwork.description);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const formData = new FormData();
+            formData.append('title', title);
+            formData.append('description', description);
+            // Append file if a new image is uploaded
+            // ...
+
+            await axios.put(`http://localhost:3000/api/artworks/${artwork.id}`, formData);
+            onSubmitSuccess();
+        } catch (error) {
+            console.error('Error updating artwork:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
+            <button type="submit">Update Artwork</button>
+        </form>
+    );
+};
